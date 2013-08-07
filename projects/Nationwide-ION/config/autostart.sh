77fff0f32595b96d11e7482a9827201c9adc9cef
davilla <davilla@4pi.com>
2013-08-06 20:46:48 -0400

    nwmn: bump addon versions, fix weserver read/write issues, add boot/xbmc splash

--- projects/Nationwide-ION/config/autostart.sh
+++ projects/Nationwide-ION/config/autostart.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+/bin/mkdir -p /storage/.xbmc/addons
+/bin/ln -s /storage/.config/webinterface.nationwide_membernet /storage/.xbmc/addons/webinterface.nationwide_membernet
+
 (
   /bin/sleep 30; \
   /usr/sbin/ntpdate pool.ntp.org; \
