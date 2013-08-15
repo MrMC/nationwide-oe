501e47bb5ece5dd972da644d58e985b379cfd3ca
davilla <davilla@4pi.com>
2013-08-15 16:27:08 -0400

    nwmn: silence error is symlink exists, redux

--- projects/Nationwide-ION/config/autostart.sh
+++ projects/Nationwide-ION/config/autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 /bin/mkdir -p /storage/.xbmc/addons
-/bin/ln -s /storage/.config/webinterface.nationwide_membernet /storage/.xbmc/addons/webinterface.nationwide_membernet 2>/dev/null
+/bin/ln -sf /storage/.config/webinterface.nationwide_membernet /storage/.xbmc/addons/webinterface.nationwide_membernet
 
 (
   /bin/sleep 30; \
