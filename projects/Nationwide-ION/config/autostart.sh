4c9bcfbe8c61738c0bb4078e6748a13ca6bf5fdb
davilla <davilla@4pi.com>
2013-09-25 15:06:17 -0400

    nwmn: bump to version 4.0.4, fixes missing audio on jc601 boxes

--- projects/Nationwide-ION/config/autostart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-/bin/mkdir -p /storage/.xbmc/addons
-/bin/ln -sf /storage/.config/webinterface.nationwide_membernet /storage/.xbmc/addons/webinterface.nationwide_membernet
-
-(
-  /bin/sleep 30; \
-  /usr/sbin/ntpdate pool.ntp.org; \
-)&
