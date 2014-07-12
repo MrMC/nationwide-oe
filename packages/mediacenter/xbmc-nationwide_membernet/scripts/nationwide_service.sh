953c74f22e763ea97c45a198142379ab42a60e4a
davilla <davilla@4pi.com>
2014-07-12 11:07:41 -0400

    nwmn: bump to nationwide-4.1.4.6, fix updating xbmc-nationwide_membernet

--- packages/mediacenter/xbmc-nationwide_membernet/scripts/nationwide_service.sh
+++ packages/mediacenter/xbmc-nationwide_membernet/scripts/nationwide_service.sh
@@ -25,7 +25,24 @@ fi
 # setup webinterface.nationwide_membernet
 # so we can get to it from the addon service
 /bin/mkdir -p /storage/.xbmc/addons
-/bin/ln -sf /storage/.config/webinterface.nationwide_membernet /storage/.xbmc/addons/webinterface.nationwide_membernet
+#
+WEBINTERFACE_SRC=/usr/config/webinterface.nationwide_membernet
+WEBINTERFACE_DST=/storage/.config/webinterface.nationwide_membernet
+/bin/ln -sf $WEBINTERFACE_DST /storage/.xbmc/addons/webinterface.nationwide_membernet
+# on 1st boot, OpenElec will populate /storage/.config/webinterface.nationwide_membernet,
+# once present, OpenElec will skip the population so we have to handle update it ourself.
+if [ -d "$WEBINTERFACE_DST" ]; then
+  # save the old settings.txt, and remove everything from old
+  mv -f $WEBINTERFACE_DST/settings.txt /tmp/settings.txt
+  rm -rf $WEBINTERFACE_DST/*
+  # copy in new, cp is setup to NOT overwrite existing files (settings.txt)
+  cp -rf $WEBINTERFACE_SRC/* $WEBINTERFACE_DST/
+  # restore old settings.txt
+  mv -f /tmp/settings.txt $WEBINTERFACE_DST/settings.txt
+  # last update the current settings.txt for any missing settings
+  # url:feed added in v4.1.5
+  /usr/bin/python /usr/share/nationwide/update_webui.py $WEBINTERFACE_DST/settings.txt
+fi
 
 # make sure time is right
 (
